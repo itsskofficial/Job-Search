dcaf094ffabf27bcebf634397f202fcfcdac1ac5
SK <sarthakkarandikar03@gmail.com>
2022-10-11 15:49:22 +0530

    added env variables

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ from selenium import webdriver
 from time import *
 from selenium.webdriver.common.by import By
 
+YOUR_PATH=""
+USERNAME=""
+PASSWORD=""
+URL=""
 
 service=Service(executable_path="D:\Skills\Python\Projects\Google Meet Bot\ChromeDrivers\win32\chromedriver.exe")
 driver=webdriver.Chrome(service=service)
 
-#The constants
-URL="https://www.linkedin.com/jobs/search?keywords=Web%20Development&location=India&locationId=&geoId=102713980&f_TPR=&f_E=1&f_JT=I&f_WT=2&position=1&pageNum=0"
 driver.get(URL)
 sleep(3)
 signin=driver.find_element_by_link_text('Sign in')
 signin.click()
 sleep(3)
 username=driver.find_element_by_id('username')
-username.send_keys('sarthakkarandikar03@gmail.com')
+username.send_keys(USERNAME)
 password=driver.find_element_by_id('password')
-password.send_keys('Billionairelife@03')
+password.send_keys(PASSWORD)
 signinbtn=driver.find_element_by_xpath('//*[@id="organic-div"]/form/div[3]/button')
 signinbtn.click()
 sleep(10)
